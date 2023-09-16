e78e2b56f37f7835e23de1b189a6f88655e373e7
Ivan <ivan.korshunov.1997@gmail.com>
2023-09-16 19:13:41 +0300

    Finish implementing subquery into SpendStatisticByDateAndNameView get queryset method

--- spend/views.py
+++ spend/views.py
@@ -3,26 +3,27 @@ from rest_framework import generics
 from revenue.models import RevenueStatistic
 from spend.models import SpendStatistic
 from spend.serializers import SpendStatisticSerializer
-from django.db.models import Sum, Subquery, OuterRef
+from django.db.models import Sum, Subquery, OuterRef, DecimalField
 
 
 class SpendStatisticByDateAndNameView(generics.ListAPIView):
     serializer_class = SpendStatisticSerializer
 
     def get_queryset(self):
+
+        total_revenue_subquery = RevenueStatistic.objects.filter(
+            spend=OuterRef("pk")
+        ).values("spend").annotate(
+            total_revenue=Sum("revenue")
+        ).values("total_revenue")[:1]
+
         queryset = SpendStatistic.objects.values("date", "name").annotate(
             total_spend=Sum("spend"),
             total_impressions=Sum("impressions"),
             total_clicks=Sum("clicks"),
             total_conversion=Sum("conversion"),
             total_revenue=Sum(
-                Subquery(
-                    RevenueStatistic.objects.filter(
-                        spend=OuterRef("pk")
-                    ).values("revenue").annotate(
-                        revenue_sum=Sum("revenue")
-                    ).values("revenue_sum")
-                )
+                Subquery(total_revenue_subquery), output_field=DecimalField()
             ),
         )
 
