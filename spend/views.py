22e40daa7e69376af84c4b9182fbb124507478e5
Ivan <ivan.korshunov.1997@gmail.com>
2023-09-19 20:54:27 +0300

    Add raw SQL implementation of SpendStatisticByDateAndNameView

--- spend/views.py
+++ spend/views.py
@@ -1,32 +1,58 @@
+from django.db import connections
 from drf_spectacular.utils import extend_schema
 from rest_framework import generics
 
-from revenue.models import RevenueStatistic
 from spend.models import SpendStatistic
 from spend.serializers import SpendStatisticSerializer
-from django.db.models import Sum, Subquery, OuterRef, DecimalField
 
 
 class SpendStatisticByDateAndNameView(generics.ListAPIView):
     serializer_class = SpendStatisticSerializer
 
+    @staticmethod
+    def create_spend_revenue_view():
+        sql_create_view = """
+                    CREATE VIEW spend_revenue AS
+                    SELECT rev.spend_id, SUM(rev.revenue) AS total_revenue
+                    FROM revenue_revenuestatistic as rev
+                    GROUP BY rev.spend_id;
+                """
+        view_exists = """
+                SELECT EXISTS(
+                    SELECT *
+                    FROM main.sqlite_master
+                    WHERE name = %s
+                );
+                """
+        with connections["default"].cursor() as cursor:
+            try:
+                cursor.execute(view_exists, ("spend_revenue",))
+                view_exists = cursor.fetchone()
+                view_exists = view_exists[0]
+                if not view_exists:
+                    cursor.execute(sql_create_view)
+            except Exception as e:
+                print(f"Error creating view: {e}")
+
     def get_queryset(self):
-        total_revenue_subquery = RevenueStatistic.objects.filter(
-            spend=OuterRef("pk")
-        ).values("spend").annotate(
-            total_revenue=Sum("revenue")
-        ).values("total_revenue")[:1]
-
-        queryset = SpendStatistic.objects.values("date", "name").annotate(
-            total_spend=Sum("spend"),
-            total_impressions=Sum("impressions"),
-            total_clicks=Sum("clicks"),
-            total_conversion=Sum("conversion"),
-            total_revenue=Sum(
-                Subquery(total_revenue_subquery), output_field=DecimalField()
-            ),
+        self.create_spend_revenue_view()
+        queryset = SpendStatistic.objects.raw(
+            """
+            SELECT
+                ss.id,
+                ss.name AS name,
+                ss.date AS date,
+                SUM(ss.spend) AS total_spend,
+                SUM(ss.impressions) AS total_impressions,
+                SUM(ss.clicks) AS total_clicks,
+                SUM(ss.conversion) AS total_conversion,
+                COALESCE(SUM(sr.total_revenue), 0) AS total_revenue
+            FROM spend_spendstatistic as ss
+            LEFT JOIN spend_revenue as sr
+            ON ss.id = sr.spend_id
+            GROUP BY name, date;
+            """
         )
-
         return queryset
 
     @extend_schema(
